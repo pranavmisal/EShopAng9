04b584eeddc86ddacb9752c8b14589af1fd9cb53
Pranav Misal <pranav.misal@definelabs.in>
2020-06-04 13:27:31 +0530

    EShop nav toggle button 104

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  isOpen: boolean = false;
 
-  ngOnInit(): void {
+  toggleNavBar() {
+    this.isOpen = !this.isOpen;
   }
 
+  ngOnInit(): void {}
 }
