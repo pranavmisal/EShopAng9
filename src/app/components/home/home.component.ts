fda25c967657522126f6222bae16cddd034395bb
Pranav Misal <pranav.misal@definelabs.in>
2020-06-04 16:41:16 +0530

    Products renders on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,44 @@
+import { Product } from './../../Interface/products.interface';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  Products: Array<Product> = [
+    {
+      Name: 'Banana',
+      Price: 3,
+      Desc: 'Fruit',
+      ProductPath: 'assets/Pics/banana.png',
+    },
+    {
+      Name: 'Kiwi',
+      Price: 3,
+      Desc: 'Fruit',
+      ProductPath: 'assets/Pics/kiwi.png',
+    },
+    {
+      Name: 'Orange',
+      Price: 3,
+      Desc: 'Fruit',
+      ProductPath: 'assets/Pics/orange.png',
+    },
+    {
+      Name: 'Strawberry',
+      Price: 3,
+      Desc: 'Fruit',
+      ProductPath: 'assets/Pics/strawberry.png',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addToCart(index) {
+    console.log('Added', this.Products[index]);
   }
-
 }
