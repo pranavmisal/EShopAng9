b76b91b25019efd283a8bd7c9c1bcb1a1e1c707d
Pranav Misal <pranav.misal@definelabs.in>
2020-06-05 14:19:25 +0530

    EShop Add user service and connect with firebase 116

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
+import { UserService } from './../../services/user.service';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,17 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
   errorMsg: string = '';
-  constructor(private as: AuthService) {}
+  constructor(
+    private as: AuthService,
+    private user: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   signup(form) {
-    console.log(form.value.email);
-    console.log(form.value.password);
-
     this.as
       .signup(form.value.email, form.value.password)
-      .then((data) => console.log(data))
+      .then((data) => {
+        this.user.addNewUser(
+          data.user.uid,
+          form.value.name,
+          form.value.address
+        );
+        this.errorMsg = '';
+        this.router.navigate(['/']);
+      })
       .catch((err) => (this.errorMsg = err));
   }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private as: AngularFirestore) {}
+
+  addNewUser(id, name, address) {
+    return this.as.doc('users/' + id).set({
+      name,
+      address,
+    });
+  }
+}
